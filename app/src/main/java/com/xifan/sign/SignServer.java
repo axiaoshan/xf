5e3f346c3e9f4072b7ce1fc0967ebe8e575543d3
axiaoshan <152137319+axiaoshan@users.noreply.github.com>
2026-07-29 21:52:49 +0800

    Update SignServer.java

--- app/src/main/java/com/xifan/sign/SignServer.java
+++ app/src/main/java/com/xifan/sign/SignServer.java
@@ -111,18 +111,6 @@ public class SignServer {
         }
     }
 
-    private static String generateSigInput(String url, String body) {
-        Uri uri = Uri.parse(url);
-        String path = uri.getPath();
-        String query = uri.getQuery();
-        if (TextUtils.isEmpty(query)) {
-            return path + "&&" + body;
-        }
-        String[] params = query.split("&");
-        Arrays.sort(params);
-        return path + "&" + TextUtils.join("&", params) + "&" + body;
-    }
-
     private static String health() {
         try {
             Class<?> kSecCls = findClass("com.kuaishou.android.security.KSecurity");
@@ -164,11 +152,14 @@ public class SignServer {
             Context ctx = getContext();
             HashMap<String, String> headers = new HashMap<>();
 
-            headers.put("Ks-Encoding", "2");
-
-            String sigInput = generateSigInput(url, body);
-
+            Class<?> encCls = findClass("com.kwai.theater.framework.network.core.encrypt.EncryptHelper");
             Class<?> weaponCls = findClass("com.kuaishou.weapon.i.WeaponHI");
+
+            if (encCls != null) {
+                Method addHeaders = encCls.getDeclaredMethod("addHeaderParams", Map.class);
+                addHeaders.invoke(null, headers);
+            }
+
             if (weaponCls != null) {
                 Method gMethod = weaponCls.getDeclaredMethod("g", Context.class);
                 Object kawObj = gMethod.invoke(null, ctx);
@@ -186,24 +177,9 @@ public class SignServer {
                 } catch (Exception ignored) {}
             }
 
-            boolean isSig3 = url.contains("/rest/e/tube/inspire");
-
-            if (isSig3) {
-                Class<?> kSecCls = findClass("com.kuaishou.android.security.KSecurity");
-                if (kSecCls != null) {
-                    Method atlasSign = kSecCls.getDeclaredMethod("atlasSign", String.class);
-                    atlasSign.setAccessible(true);
-                    Object sig3Obj = atlasSign.invoke(null, sigInput);
-                    if (sig3Obj != null) headers.put("Ks-Sig3", sig3Obj.toString());
-                }
-            } else {
-                Class<?> sig1Cls = findClass("com.yxcorp.kuaishou.addfp.KWEGIDDFP");
-                if (sig1Cls != null) {
-                    Method doSign = sig1Cls.getDeclaredMethod("doSign", Context.class, String.class);
-                    doSign.setAccessible(true);
-                    Object sig1Obj = doSign.invoke(null, ctx, sigInput);
-                    if (sig1Obj != null) headers.put("Ks-Sig1", sig1Obj.toString());
-                }
+            if (encCls != null) {
+                Method sigMethod = encCls.getDeclaredMethod("sigRequest", String.class, Map.class, String.class);
+                sigMethod.invoke(null, url, headers, body);
             }
 
             JSONObject result = new JSONObject();
